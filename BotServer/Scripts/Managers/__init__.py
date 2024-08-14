33e7c6ee3984562cde42c152d21ea80749677adb
LonelySail <xiaocaicai_email@sina.com>
2024-08-14 12:19:30 +0800

    初始提交

--- BotServer/Scripts/Managers/__init__.py
+++ BotServer/Scripts/Managers/__init__.py
@@ -2,3 +2,4 @@ from .Data import data_manager
 from .Environment import environment_manager
 from .Server import server_manager
 from .Version import version_manager
+from .Lagrange import lagrange_manager
