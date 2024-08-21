25209361208534973d9d7a9d6ed140f537f5751b
LonelySail <xiaocaicai_email@sina.com>
2024-08-21 12:25:39 +0800

    修复：完善图片渲染。

--- BotServer/Scripts/Render.py
+++ BotServer/Scripts/Render.py
@@ -1,14 +1,16 @@
 from pathlib import Path
 
 from nonebot import require
+from nonebot.adapters.onebot.v11 import MessageSegment
 
 require('nonebot_plugin_htmlrender')
 from nonebot_plugin_htmlrender import template_to_pic
 
-template_path = str(Path('Resources/Images/'))
+template_path = str(Path('Resources/Images/').absolute())
 
 
 async def render_template(template_name: str, size: tuple, **kwargs):
     width, height = size
-    page = {'viewport': {'width': width, 'height': height}, 'base_url': F'file://{template_path}'}
-    return await template_to_pic(template_path, template_name, kwargs, page, 5)
+    page = {'viewport': {'width': width, 'height': height}, 'base_url': 'file://' + template_path}
+    image = await template_to_pic(template_path, template_name, kwargs, pages=page, wait=5)
+    return MessageSegment.image(image)
