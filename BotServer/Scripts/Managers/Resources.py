1b34a061d1db16680b658cca100bcc0b676ad64b
LonelySail <xiaocaicai_email@sina.com>
2024-09-16 14:58:14 +0800

    修改：更新资源管理器。

--- BotServer/Scripts/Managers/Resources.py
+++ BotServer/Scripts/Managers/Resources.py
@@ -1,41 +1,42 @@
 import sys
 from pathlib import Path
-from tempfile import gettempdir
 from zipfile import ZipFile
 
 from nonebot.log import logger
 
 
 class ResourcesManager:
-    path: Path = (Path(gettempdir()) / 'BotResources')
+    path: Path = Path('.Cache/Resources')
 
     def init(self):
+        resources_path = Path('Resources')
+        if resources_path.exists():
+            self.path = resources_path
+            logger.debug('检测到从源码运行，无需提取资源文件。')
+            return None
         logger.debug('正在初始化资源管理器……')
         if not self.path.exists():
             logger.info('未找到资源文件，正在提取……')
-            self.path.mkdir()
+            self.path.mkdir(parents=True)
             self.extract()
         logger.success('资源管理器初始化完成！')
 
     def remove(self):
-        self.path.rmdir()
+        for child in self.path.rglob('*'):
+            child.unlink()
+        self.path.unlink()
 
     def read_file(self, file_path: str):
         file_path = (self.path / file_path)
         return file_path.read_text('Utf-8')
 
     def extract(self):
+        cache_path = self.path.parent
         with ZipFile(sys.argv[0], 'r') as zip_file:
             for file in zip_file.namelist():
-                if file.startswith('Resources/') and (file != 'Resources/'):
-                    file_path = file[10:]
-                    print(file_path)
-                    target_path = (self.path / file_path)
-                    if '.' in file_path:
-                        with target_path.open('wb') as target_file:
-                            target_file.write(zip_file.read(file))
-                        continue
-                    target_path.mkdir()
+                if file.startswith('Resources/'):
+                    zip_file.extract(file, cache_path)
+                    continue
 
 
 resources_manager = ResourcesManager()
